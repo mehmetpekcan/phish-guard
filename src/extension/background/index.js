2d12ccd2d230bb51e4c345e383d7fe94658aa1d2
mehmetpekcan <mpekcan@atlassian.com>
2023-03-05 18:12:35 +0300

    Connect domainSimilarity service

--- src/extension/background/index.js
+++ src/extension/background/index.js
@@ -10,6 +10,25 @@ const ICON_PATHS = {
   [STATUS.WARN]: "/icons/icon-warn.png",
 };
 
+const BASE_URL = "http://api.phishguard.cc";
+
+async function getDomainSimilarity(hostname) {
+  const STATUS_CODES = {
+    100: STATUS.SECURE,
+    200: STATUS.INSECURE,
+    201: STATUS.INSECURE,
+  };
+
+  const searchParams = new URLSearchParams({ current_domain: hostname });
+
+  const response = await fetch(
+    `${BASE_URL}/api/analyse/domain-similarity?${searchParams}`
+  );
+  const { status_code: statusCode } = await response.json();
+
+  return { status: STATUS_CODES[statusCode] ?? STATUS.WARN };
+}
+
 function changeExtensionIcon(tabId, status) {
   chrome.action.setIcon({ path: ICON_PATHS[status], tabId });
 }
@@ -27,6 +46,16 @@ async function handleTabUpdated(tabId, { status }, tab) {
     }
 
     setTimeout(async () => {
+      // const { status: domainStatus } = await getDomainSimilarity(hostname);
+      // const domainStatus = STATUS.INSECURE
+
+      if (domainStatus === STATUS.SECURE) {
+        // TODO: dispatch success events
+      } else if (domainStatus === STATUS.WARN) {
+        // TODO:directly finish evertything end dispatch warn events
+      } else if (domainStatus === STATUS.INSECURE) {
+      }
+
       const image = await chrome.tabs.captureVisibleTab();
 
       // TODO: Send image to server
